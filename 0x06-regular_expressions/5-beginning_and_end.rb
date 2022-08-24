f1d3e4fb143f99e08e18146c91739cbdff5021f9
DammyD <ibukunoluwadawodu@gmail.com>
2022-08-24 05:23:45 -0700

    Task 5-6

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# The regular expression that match a string with h and n
+# The regular expression that match a string that starts with h and ends with n
 puts ARGV[0].scan(/^h.n$/).join
